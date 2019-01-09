08ea378ddf89f4cce46d309337527dd71d688b13
vishal31raj <vraj07909@gmail.com>
2019-01-09 21:27:10 +0530

    Greet new user and alert others

--- public/js/index.js
+++ public/js/index.js
@@ -3,14 +3,14 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server.');
-
 });
+
 socket.on('disconnect', function () {
     console.log('Disconnected from server.');
 });
 
 socket.on('newMessage', function(message) {
-    console.log('Got new message.', message);
+    console.log(message);
 });
 
 
